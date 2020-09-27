bd81f208fba9ee319d3540e4c9b7b054729ffa5e
Felix Husen <felix@idlprojections.com>
2020-09-27 22:19:27 +1000

    Fix android x plugin

--- src/android/hr/antoniostipic/MlKitPlugin.java
+++ src/android/hr/antoniostipic/MlKitPlugin.java
@@ -11,7 +11,7 @@ import org.json.JSONException;
 import android.net.Uri;
 import android.content.Context;
 import android.graphics.Rect;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.graphics.Point;
 
 import android.util.Log;
