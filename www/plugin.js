7c9202bb83a489cbeab95146e819389606419c25
fh997 <fh997@uowmail.edu.au>
2020-09-28 20:19:15 +1000

    Fix js

--- www/plugin.js
+++ www/plugin.js
@@ -1,18 +1,19 @@
-
-var exec = require('cordova/exec');
-var PLUGIN_NAME = 'MlKitPlugin';
+const cordova = require("cordova");
+const PLUGIN_NAME = "MlKitPlugin";
 
 module.exports = {
-  getText: function (img, options, success, error) {
-    exec(success, error, PLUGIN_NAME, 'getText', [img]);
+  getText: (img, options, success, error) => {
+    cordova.exec(success, error, PLUGIN_NAME, "getText", [img]);
   },
-  getTextCloud: function (img, options, success, error) {
+  getTextCloud: (img, options, success, error) => {
     options = options || {};
-    var language = options.language || '';
-    exec(success, error, PLUGIN_NAME, 'getTextCloud', [img, language]);
-  }, getLabel: function (img, options, success, error) {
-    exec(success, error, PLUGIN_NAME, 'getLabel', [img]);
-  }, getLabelCloud: function (img, options, success, error) {
-    exec(success, error, PLUGIN_NAME, 'getLabelCloud', [img]);
+    const language = options.language || "";
+    cordova.exec(success, error, PLUGIN_NAME, "getTextCloud", [img, language]);
+  },
+  getLabel: (img, options, success, error) => {
+    cordova.exec(success, error, PLUGIN_NAME, "getLabel", [img]);
+  },
+  getLabelCloud: (img, options, success, error) => {
+    cordova.exec(success, error, PLUGIN_NAME, "getLabelCloud", [img]);
   },
 };
